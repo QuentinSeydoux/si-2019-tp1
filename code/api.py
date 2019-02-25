ac77819f13a963e71c93a6a0dd62a33124653949
Quentin Seydoux <quentin.seydoux@gmail.com>
2019-02-25 16:34:45 +0100

    This commit will begin the counter to end the world.

--- code/api.py
+++ code/api.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, World! This is the best comment ever !"
 
 if __name__ == '__main__':
     app.run(debug=True)
